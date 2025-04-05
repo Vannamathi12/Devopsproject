7a66e833861ca0d82f7a2611fec1d363c76bee66
Vannamathi <127740426+Vannamathi12@users.noreply.github.com>
2025-04-05 09:28:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u vannamathi124 -p mathi@123
+    docker tag test vannamathi124/devopsproject
+    docker push vannamathi124/devopsproject
+   
     
